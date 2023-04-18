28df148a0138b6aaef84bef04606df57a5655e4b
ryanmotamen <ryanmotamen@gmail.com>
2023-04-18 11:28:06 -0700

    set up the back-end
    Co-authored-by: Michael Gacetta <michael.gacetta@gmail.com>
    Co-authored-by: Nattie Chan <nattie.chan@gmail.com>
    Co-authored-by: Eileen Cho <109772371+exlxxn@users.noreply.github.com>
    Co-authored-by: Derrick Oh  <juneparkoh@gmail.com>

---
+++ server/controllers/swatchController.js
@@ -0,0 +1,8 @@
+// import db from "../models/swatchModel";
+
+export const swatchController = {};
+
+swatchController.test = (req, res, next) => {
+  console.log('running swatchController.testNow!');
+  return next();
+}

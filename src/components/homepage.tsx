a67f8b4a3900151d376e57eea515f528059def6b
Nattie C <47332368+nattiechan@users.noreply.github.com>
2023-04-18 10:43:52 -0700

    Setup basic infrastructure for front end

---
+++ src/components/homepage.tsx
@@ -0,0 +1,10 @@
+function HomePage(): JSX.Element {
+  return (
+    <div className='App'>
+      <h1>Hello World!</h1>
+      <h3 className='test'>Here</h3>
+    </div>
+  );
+}
+
+export default HomePage;

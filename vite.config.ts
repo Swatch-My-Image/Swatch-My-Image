2dea3baa288bf9ed4ae076c4057de29ec19918d8
ryanmotamen <ryanmotamen@gmail.com>
2023-04-19 10:08:18 -0700

    set up ENV and first query

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+// import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
+
+// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({

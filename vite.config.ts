e1c14e9b29b887a555a5fdc6782a61a4c9f95b9f
ryanmotamen <ryanmotamen@gmail.com>
2023-04-19 10:31:17 -0700

    cleaneed up vite comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-// import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
-
-// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
